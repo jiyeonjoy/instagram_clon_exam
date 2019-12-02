ff55f778cd63c2372cb9fbf0235ab0c14d9a8741
jiyeonjoy <choi02647@naver.com>
2019-12-03 07:49:29 +0900

    로그인 페이지 UI 작성

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 // 상태 변하지 않으므로 StatelessWidget 으로 생성
 // stless 입력하고 엔터치면 됨!!
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('login page'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Instagram Clon',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.all(50.0),
+            ),
+            SignInButton(
+              Buttons.Google, onPressed: () {
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
